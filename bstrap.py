eb0fddab7cad17a5408f4297fe84ef9ed2d0f934
Nam Phan <namphan00@gmail.com>
2016-08-15 17:31:53 -0500

    added grid and some typography examples

--- bstrap.py
+++ bstrap.py
@@ -5,3 +5,11 @@ app.config.from_object(__name__)
 @app.route("/")
 def index():
     return render_template('home.html')
+
+@app.route("/grid")
+def grid():
+    return render_template('grid.html')
+
+@app.route("/grid_typo")
+def grid_typo():
+    return render_template('grid_typo.html')
